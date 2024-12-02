07df337709278560de5a1d6895fdc34d6cca9f6c
yoshi_mac <yosshiy5937@gmail.com>
2024-12-03 01:55:47 +0900

    update models.go for omiempty

--- model/models.go
+++ model/models.go
@@ -5,10 +5,10 @@ import "time"
 // User モデル
 type User struct {
 	UserID        string     `json:"user_id"`
-	Name          string     `json:"name"`
-	Bio           string     `json:"bio"`
-	ProfileImgURL string     `json:"profile_img_url"`
-	HeaderImgURL  string     `json:"header_img_url"`
+	Name          string     `json:"name,omitempty"`
+	Bio           string     `json:"bio,omitempty"`
+	ProfileImgURL string     `json:"profile_img_url,omitempty"`
+	HeaderImgURL  string     `json:"header_img_url,omitempty"`
 	Location      string     `json:"location"`
 	Birthday      *time.Time `json:"birthday,omitempty"`
 	TweetCount    int        `json:"tweet_count,omitempty"`
@@ -20,7 +20,7 @@ type Post struct {
 	PostID       string     `json:"post_id"`
 	UserID       string     `json:"user_id"`
 	Content      string     `json:"content"`
-	ImgURL       string     `json:"img_url"`
+	ImgURL       string     `json:"img_url,omitempty"`
 	CreatedAt    time.Time  `json:"created_at"`
 	EditedAt     *time.Time `json:"edited_at,omitempty"`
 	DeletedAt    *time.Time `json:"deleted_at,omitempty"`
@@ -30,7 +30,7 @@ type Post struct {
 // Like モデル
 type Like struct {
 	UserID string `json:"user_id"`
-	PostID string `json:"post_id,omitempty"`
+	PostID string `json:"post_id"`
 }
 
 // Follow モデル
